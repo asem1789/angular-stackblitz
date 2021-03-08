99e8ae34a97931b31a9b937cc32c5c9e701e9f82
asem1789 <as.1789@hotmail.com>
2021-03-08 05:37:34 +0200

    add a question in a comment

--- src/app/components/film-details/film-details.component.ts
+++ src/app/components/film-details/film-details.component.ts
@@ -10,7 +10,24 @@ export class FilmDetailsComponent  implements OnInit {
   @Input() film!: Film;
   // @Input() film: Film;
 
-  constructor() {}
+  /*
+
+    * if I comment line 10, uncomment line 11, will occur error : "Property 'film' has no initializer and is not definitely assigned in the constructor."
+    * To see my screen Error, you can click on "Show screen Error" in right top of page,
+  
+    * in stackblitz site you will no notice Error, I dont know why, but locally I have
+
+    * I can skipt this error as above Or change these varaibles "noImplicitReturns" & "strictPropertyInitialization"  in tsconfig.json to false
+
+    #Questions:
+    @ how can I handle this error without skip it
+
+
+  */
+
+
+  constructor() {
+  }
   
   ngOnInit() {
     // console.log(this.film);
